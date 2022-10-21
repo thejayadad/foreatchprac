5f6472ca63069779f3195a758312914b5399c1f8
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-20 20:43:17 -0400

    update

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 
 let transactions = [200, 250, -100, 300, -500, 600]
-let box = document.getElementById('box')
+
+let box = document.getElementById("box")
 
 let activities = function (transactions){
-    transactions.forEach(function (el, i) {
+    transactions.forEach(function(el) {
         let type = el > 0 ? "Deposit" : "Withdrawl"
         let html = `
+
         <div class="row">
-        <div class="type">
-            ${type}
+        <div>
+        ${type}
         </div>
-        <div class="amount">
-            ${el}
-            </div>
+        <div>
+        ${el}
         </div>
-
+        </div>
         `;
-
         box.insertAdjacentHTML("afterbegin", html)
     })
 }
-
 activities(transactions)
